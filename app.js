b4cd5aecdfd4728909051ba99e816f67c7fc28ab
Wesley Selmer <wesleyselmer@gmail.com>
2024-02-07 13:31:29 -0300

    Parte Lógica só falta remover acentos

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function limparTexto(){
-    document.querySelector('input').value = '';
+    document.querySelector('textarea').value = '';
 };
 
 function criptografar(){
-    let textoASerCriptografado = document.querySelector('input').value.toLowerCase();
+    let textoASerCriptografado = document.querySelector('textarea').value.toLowerCase();
     if (textoASerCriptografado){
         textoASerCriptografado = textoASerCriptografado.replace('e', 'enter');
         textoASerCriptografado = textoASerCriptografado.replace('i', 'imes');
@@ -11,11 +11,13 @@ function criptografar(){
         textoASerCriptografado = textoASerCriptografado.replace('o', 'ober');
         textoASerCriptografado = textoASerCriptografado.replace('u', 'ufat');
         escreverResultado(textoASerCriptografado);
+        document.getElementById("btn_copiar").removeAttribute('hidden');
+        limparTexto();
     }
 }
 
-function criptografar(){
-    let textoASerDescriptografado = document.querySelector('input').value.toLowerCase();
+function descriptografar(){
+    let textoASerDescriptografado = document.querySelector('textarea').value.toLowerCase();
     if (textoASerDescriptografado){
         textoASerDescriptografado = textoASerDescriptografado.replace('enter', 'e');
         textoASerDescriptografado = textoASerDescriptografado.replace('imes', 'i');
@@ -23,10 +25,30 @@ function criptografar(){
         textoASerDescriptografado = textoASerDescriptografado.replace('ober', 'o');
         textoASerDescriptografado = textoASerDescriptografado.replace('ufat', 'u');
         escreverResultado(textoASerDescriptografado);
+        document.getElementById("btn_copiar").removeAttribute('hidden');
+        limparTexto();
     }
 }
 function escreverResultado(texto){
-    document.getElementById("result__image").setAttribute('hidden','');
-    document.getElementById('result__message').setAttribute('hidden','');
-    document.getElementById('result__message_title').innerHTML = texto;
+    document.getElementById("result__img").setAttribute('hidden','');
+    document.getElementById('result__p').setAttribute('hidden','');
+    document.getElementById('result__h2').innerHTML = texto;
+}
+
+function copiar(){
+    let textoCopia = document.getElementById('result__h2').innerText;
+    if(textoCopia) {
+        navigator.clipboard.writeText(textoCopia)
+        .then(() => {
+            alert("Texto copiado para a área de transferência!");
+            // Adicionando um pequeno atraso antes de chamar exibirResultado
+            setTimeout(() => {
+                exibirResultado('');
+            }, 100);
+            document.getElementById('result__h2').innerText = 'Nenhuma mensagem encontrada';
+            document.getElementById('result__p').removeAttribute('hidden');
+            document.getElementById('result__img').removeAttribute('hidden');
+            document.getElementById('btn_cpoiar').setAttribute('hidden','');
+        })
+    }
 }
